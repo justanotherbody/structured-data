047095430b1eeda79345c6ee32320ec840e360a4
Mike Richards <justanotherbody@gmail.com>
2014-07-04 08:45:25 -0700

    First ~half of chapter 3

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,21 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [x2 (+ x x)]
+    (Math/pow x2 x2)
+    ))
 
 (defn spiff [v]
-  :-)
+  (let [one (get v 0)
+        three (get v 2)]
+    (+ one three)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[one, UNUSED, three] v]
+    (+ one three)))
 
 (defn point [x y]
   [x y])
@@ -19,37 +24,44 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1, _] [x2 _]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[_, y1] [_ y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (= (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1, y1] [x2, y2]] rectangle
+        [px, py] point]
+    (and (<= x1 px x2) (<= y1 py y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[inner-bottom-left inner-top-right] inner]
+    (and (contains-point? outer inner-bottom-left) (contains-point? outer inner-top-right))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> 1 (author-count book)))
 
 (defn add-author [book new-author]
-  :-)
+  (let [authors (:authors book)
+        updated-authors (conj authors new-author)]
+    (assoc book :authors updated-authors)))
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year)))
 
 (defn element-lengths [collection]
   :-)
